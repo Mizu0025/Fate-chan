76a83fc9ab72e118b84078d9f111b6fc590b3990
Liam Ash <liamash3@gmail.com>
2024-03-15 22:25:48 +1100

    add basic irc connectivity

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    node: true,
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint", "prettier"],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"],
+  rules: {
+    "prettier/prettier": "error",
+  },
+};
